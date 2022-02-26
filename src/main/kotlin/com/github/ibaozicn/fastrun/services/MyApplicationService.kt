11edb078968f4bacbf9fb79447e6ec212c34c018
GitHub Action <action@github.com>
2022-02-26 05:35:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/ibaozicn/fastrun/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ibaozicn/fastrun/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ibaozicn.fastrun.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ibaozicn.fastrun.MyBundle
 
 class MyApplicationService {
 
